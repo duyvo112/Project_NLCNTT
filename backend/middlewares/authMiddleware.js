848b333810c0c862ca43cc50445c4bd9af64f082
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-11 12:05:56 +0700

    tạo thêm 2 middle

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -20,13 +20,46 @@ const authMiddleware = {
       },
       verifyTokenAndUserAuthorization: (req, res, next) => {
         authMiddleware.verifyToken(req, res, () => {
-          if (req.user.id === req.params.id.trim() || req.user.isAdmin) {
+          if (req.user.id === req.params.id || req.user.isAdmin) {
             next();
           } else {
             return res.status(403).json("You're not allowed to do that!");
           }
         });
       },
+      verifyTokenAndUserPostAuthorization: async (req, res, next) => {
+        authMiddleware.verifyToken(req, res, async () => {
+            try {
+                const post = await Post.findById(req.params.id);
+                if (!post) {
+                    return res.status(404).json("Post not found");
+                }
+                console.log(req.user.isAdmin);
+                if (post.user.toString() === req.user.id || req.user.isAdmin) {
+                    next();
+                } else {
+                    return res.status(403).json("You're not allowed to do that!");
+                }
+            } catch (error) {
+                return res.status(500).json("Internal Server Error");
+            }
+        });
+    },
+     verifyTokenAndCommentAuthorization: (req, res, next) => {
+    middlewareController.verifyToken(req, res, () => {
+      console.log("req.user.id: " + req.user.id);
+      console.log("ownerId: :" + req.body.ownerId);
+      if (
+        req.user.id === req.body.ownerId ||
+        req.user.isAdmin ||
+        req.user.id === req.body.postId
+      ) {
+        next();
+      } else {
+        return res.status(403).json("You're not allowed to do that!");
+      }
+    });
+  },
       
 }
 module.exports = authMiddleware;
