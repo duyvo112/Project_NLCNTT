187f595e10a55bf199825d810af393426ae7f723
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-21 20:20:45 +0700

    them piniaPlugin va fontaS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,26 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from './App.vue'
 import router from './router/index'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+// Import Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Thêm tất cả các icon vào thư viện
+library.add(fas)
+
 const app = createApp(App)
 
-app.use(router).use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router).use(pinia)
 
 app.mount('#app')
