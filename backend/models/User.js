d75e5dd7b861a689d763da9d5bcd16e07973b615
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-23 19:28:20 +0700

    sua avatar mặc định

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,12 +2,17 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  fullname: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   about: { type: String, default: "I'm a new user" }, // Giới thiệu bản thân
   address: { type: String, default: "" }, // Địa chỉ
   phone: { type: String, default: "" }, // Số điện thoại
-  avatar: { type: String, default: "https://preview.redd.it/rrz3hmsxcll71.png?width=640&crop=smart&auto=webp&s=87cc5ed38d8f088ef9fffef7a4c5756b64309d6a" }, // Ảnh đại diện
+  avatar: {
+    type: String,
+    default:
+      "https://t4.ftcdn.net/jpg/00/97/00/09/240_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg",
+  }, // Ảnh đại diện
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Danh sách bạn bè
   friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Lời mời kết bạn
   isAdmin: {
