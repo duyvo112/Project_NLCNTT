554a4bdeed9db34826efa5a543c5eeeeabd7c6d5
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-23 19:33:23 +0700

    thêm các api

--- frontend/src/services/socialMediaApi.service.js
+++ frontend/src/services/socialMediaApi.service.js
@@ -1,13 +1,19 @@
-import axiosInstance from "./api.service";
+import axiosInstance from './api.service'
 
 const socialMediaApi = {
-    //Authentication
-    login: (data) => axiosInstance.post("/api/auth/login", data),
-    register: (data) => axiosInstance.post("/api/auth/register", data),
-    logout: () => axiosInstance.post("/api/auth/logout"),
-    refreshToken: () => axiosInstance.post("/api/auth/refresh"),
-    //User
-    
-};
+  //Authentication
+  login: (data) => axiosInstance.post('/api/auth/login', data),
+  register: (data) => axiosInstance.post('/api/auth/register', data),
+  logout: () => axiosInstance.post('/api/auth/logout'),
+  refreshToken: () => axiosInstance.post('/api/auth/refresh'),
+  //User
+  getUser: (id) => axiosInstance.get(`/api/user/${id}`),
+  //Posts
+  getPosts: () => axiosInstance.get('/api/post'),
+  getPostbyUser: (id) => axiosInstance.get(`/api/post/user/${id}`),
+  likePost: (id) => axiosInstance.put(`/api/post/like/${id}`),
+  //Friends
+  getFriends: (id) => axiosInstance.get(`/api/user/friends/${id}`),
+}
 
-export default socialMediaApi;
+export default socialMediaApi
