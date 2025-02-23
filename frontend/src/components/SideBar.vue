301edb60b28c14e2b03572d2b34db7502d0752e9
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-23 19:32:31 +0700

    thêm router link

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,27 +1,53 @@
 <template>
     <div class="sidebar p-3">
-        <h2 class="text-center mb-4 logo">FriendGram</h2>
+        <h2 class="text-center logo">FriendGram</h2>
         <ul class="nav flex-column fs-5">
-            <li class="nav-item m-1"><a class="nav-link" href="#"><font-awesome-icon class="me-2"
-                        :icon="['fas', 'house']" />Home</a>
+            <li class="nav-item m-1">
+                <router-link class="nav-link" :to="{ name: 'HomePage' }">
+                    <font-awesome-icon class="me-2" :icon="['fas', 'house']" />Home
+                </router-link>
             </li>
-            <li class="nav-item m-1"><a class="nav-link" href="#"><font-awesome-icon class="me-2"
-                        :icon="['fas', 'magnifying-glass']" />Search</a></li>
-            <li class="nav-item m-1"><a class="nav-link" href="#"><font-awesome-icon class="me-2"
-                        :icon="['fas', 'compass']" />Explore</a></li>
-            <li class="nav-item m-1"><a class="nav-link" href="#"><font-awesome-icon class="me-2"
-                        :icon="['fas', 'message']" />Messages</a></li>
-            <li class="nav-item m-1"><a class="nav-link" href="#"><font-awesome-icon class="me-2"
-                        :icon="['fas', 'bell']" />Notifications</a></li>
-            <li class="nav-item m-1"><a class="nav-link" href="#"><font-awesome-icon class="me-2"
-                        :icon="['fas', 'user']" />Profile</a></li>
+            <!-- <li class="nav-item m-1">
+                <router-link class="nav-link" :to="{ name: 'Search' }">
+                    <font-awesome-icon class="me-2" :icon="['fas', 'magnifying-glass']" />Search
+                </router-link>
+            </li>
+            <li class="nav-item m-1">
+                <router-link class="nav-link" :to="{ name: 'Explore' }">
+                    <font-awesome-icon class="me-2" :icon="['fas', 'compass']" />Explore
+                </router-link>
+            </li>
+            <li class="nav-item m-1">
+                <router-link class="nav-link" :to="{ name: 'Messages' }">
+                    <font-awesome-icon class="me-2" :icon="['fas', 'message']" />Messages
+                </router-link>
+            </li>
+            <li class="nav-item m-1">
+                <router-link class="nav-link" :to="{ name: 'Notifications' }">
+                    <font-awesome-icon class="me-2" :icon="['fas', 'bell']" />Notifications
+                </router-link>
+            </li>
+            <li class="nav-item m-1">
+                <router-link class="nav-link" :to="{ name: 'UserProfile', params: { id: userId } }">
+                    <font-awesome-icon class="me-2" :icon="['fas', 'user']" />Profile
+                </router-link>
+            </li> -->
         </ul>
     </div>
 </template>
 
 <script>
+import { computed } from "vue";
+import { useUserStore } from "@/stores/userStore";
+
 export default {
-    name: 'SideBar'
+    name: "SideBar",
+    setup() {
+        const userStore = useUserStore();
+        const userId = computed(() => userStore.user?._id); // Lấy ID người dùng từ store
+
+        return { userId };
+    },
 };
 </script>
 
@@ -37,6 +63,7 @@ export default {
 
 .nav-link {
     color: black;
+    text-decoration: none;
 }
 
 .nav-link:hover {
