e4c2710f58548784455f85441ac4dcb16b753196
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-23 19:28:50 +0700

    getAllPost từ Feed

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,30 +1,33 @@
 <template>
-    <NewPostForm @post-submitted="handleNewPost" />
-    <hr />
-    <div class="h1">NewsFeed</div>
-    <div class="feed w-85 container">
-        <Post v-for="post in posts" :key="post.id" :post="post" />
+    <div>
+        <NewPostForm @post-submitted="handleNewPost" />
+        <hr />
+        <div class="h1">NewsFeed</div>
+        <div class="feed w-85 container">
+            <Post v-for="post in posts" :key="post.id" :post="post" />
+        </div>
     </div>
-
 </template>
 
 <script>
 import Post from "./Post.vue";
+import { usePostStore } from '../stores/postStore'
+import { computed, onMounted } from 'vue'
 import NewPostForm from "./NewPostForm.vue";
 export default {
     components: { Post, NewPostForm },
     name: 'NewsFeed',
-    data() {
-        return {
-            posts: [
-                { id: 1, user: "Alice", image: "https://res.cloudinary.com/dpl7e9iqw/image/upload/v1739501070/mini-social-network/posts/jmtu0uvtaaxcepmhyeyt.png", caption: "Hello world!", likes: 10 },
-                { id: 2, user: "Bob", image: "https://res.cloudinary.com/dpl7e9iqw/image/upload/v1739501070/mini-social-network/posts/jmtu0uvtaaxcepmhyeyt.png", caption: "Vue.js is awesome!", likes: 25 },
-                { id: 2, user: "Bob", image: "https://res.cloudinary.com/dpl7e9iqw/image/upload/v1739501070/mini-social-network/posts/jmtu0uvtaaxcepmhyeyt.png", caption: "Vue.js is awesome!", likes: 25 },
-                { id: 2, user: "Bob", image: "https://res.cloudinary.com/dpl7e9iqw/image/upload/v1739501070/mini-social-network/posts/jmtu0uvtaaxcepmhyeyt.png", caption: "Vue.js is awesome!", likes: 25 },
-                { id: 2, user: "Bob", image: "https://res.cloudinary.com/dpl7e9iqw/image/upload/v1739501070/mini-social-network/posts/jmtu0uvtaaxcepmhyeyt.png", caption: "Vue.js is awesome!", likes: 25 },
-            ],
-        };
-    },
+    setup() {
+        const postStore = usePostStore();
+        const posts = computed(() => postStore.posts);
+
+        // ✅ Tải danh sách tất cả bài viết khi component mount
+        onMounted(() => {
+            postStore.getPosts();
+        });
+
+        return { posts };
+    }
 };
 </script>
 
