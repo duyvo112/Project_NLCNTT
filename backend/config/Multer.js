543957b832c5880752615da029b9ef0e5608d6db
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-24 12:38:23 +0700

    save

--- backend/config/Multer.js
+++ backend/config/Multer.js
@@ -3,13 +3,13 @@ const cloudinary = require("../config/Cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-      folder: "mini-social-network/posts",
-      allowed_formats: ["jpg", "png"],
-      transformation: [{ width: 500, height: 500, crop: "limit" }],
-    },
-  });
-  const upload = multer({ storage });
-  
+  cloudinary,
+  params: {
+    folder: "mini-social-network/posts",
+    allowed_formats: ["jpg", "png"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }],
+  },
+});
+const upload = multer({ storage });
+
 module.exports = upload;
