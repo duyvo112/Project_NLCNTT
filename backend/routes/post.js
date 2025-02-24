ecb2e7fd1bae8f1562dd172d5af4d6f23236ed03
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-24 12:39:23 +0700

    format

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,47 +1,80 @@
-const postController = require('../controllers/postController');
-const upload = require('../config/Multer');
-const router = require('express').Router();
-const authMiddleware = require('../middlewares/authMiddleware');
-const commentController = require('../controllers/commentController');
+const postController = require("../controllers/postController");
+const upload = require("../config/Multer");
+const router = require("express").Router();
+const authMiddleware = require("../middlewares/authMiddleware");
+const commentController = require("../controllers/commentController");
 
 /*---------------POST ROUTES-------------------*/
 
 // Create Post
-router.post('/',authMiddleware.verifyToken,upload.single("image"), postController.createPost);
+router.post(
+  "/",
+  authMiddleware.verifyToken,
+  upload.single("image"),
+  postController.createPost
+);
 
 // Get Post by Friends
 
-router.get('/friends',authMiddleware.verifyToken, postController.getPostByFriends);
+router.get(
+  "/friends",
+  authMiddleware.verifyToken,
+  postController.getPostByFriends
+);
 
 // Get All Posts
-router.get('/',authMiddleware.verifyToken, postController.getAllPosts);
+router.get("/", authMiddleware.verifyToken, postController.getAllPosts);
 
 // Get Post by ID
-router.get('/:id',authMiddleware.verifyToken, postController.getPost);
+router.get("/:id", authMiddleware.verifyToken, postController.getPost);
 
 // Delete Post
-router.delete('/:id',authMiddleware.verifyTokenAndUserPostAuthorization, postController.deletePost);
+router.delete(
+  "/:id",
+  authMiddleware.verifyTokenAndUserPostAuthorization,
+  postController.deletePost
+);
 
 // Update Post
-router.put('/:id',authMiddleware.verifyTokenAndUserPostAuthorization,upload.single("image"), postController.updatePost);
+router.put(
+  "/:id",
+  authMiddleware.verifyTokenAndUserPostAuthorization,
+  upload.single("image"),
+  postController.updatePost
+);
 
 // Like Post
-router.put('/like/:id',authMiddleware.verifyToken, postController.likePost);
+router.put("/like/:id", authMiddleware.verifyToken, postController.likePost);
 
 // Get Posts by User
-router.get('/user/:id',authMiddleware.verifyToken, postController.getPostByUser);
-
+router.get(
+  "/user/:id",
+  authMiddleware.verifyToken,
+  postController.getPostByUser
+);
 
 /*---------------COMMENT ROUTES-------------------*/
 
 // Comment on Post
-router.put('/comment/:id',authMiddleware.verifyToken, commentController.commentPost);
+router.put(
+  "/comment/:id",
+  authMiddleware.verifyToken,
+  commentController.commentPost
+);
 
 // Get Comments
-router.get('/comment/:id',authMiddleware.verifyToken, commentController.getComments);
+router.get(
+  "/comments/:id",
+  authMiddleware.verifyToken,
+  commentController.getComments
+);
 
 // Delete Comment
-router.delete('/comment/:id',authMiddleware.verifyToken, commentController.deleteComment);
+router.delete(
+  "/comment/:id",
+  authMiddleware.verifyToken,
+  commentController.deleteComment
+);
 
 /*-----------------------------------------------*/
 
