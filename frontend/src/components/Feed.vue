1becc66a384ebc7b52c60f47f3ea87e95bc332c1
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-25 16:12:58 +0700

    delete handle

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <NewPostForm class="w-100" @post-submitted="handleNewPost" />
+        <NewPostForm class="w-100" />
         <hr />
         <div class="h1">NewsFeed</div>
         <div class="feed w-85 container">
