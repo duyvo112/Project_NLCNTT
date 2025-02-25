319cadf1b923a4acfc7bf65f51375a9954699a14
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-25 22:57:40 +0700

    thêm explore

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 const postRoutes = require("./post");
 const userRoutes = require("./user");
 const authRoutes = require("./auth");
+const exploreRoutes = require("./explore");
 
-const InitRoutes = (app) => { 
-    app.use("/api/post",postRoutes);
-    app.use("/api/user", userRoutes);
-    app.use("/api/auth", authRoutes);
-}
+const InitRoutes = (app) => {
+  app.use("/api/post", postRoutes);
+  app.use("/api/user", userRoutes);
+  app.use("/api/auth", authRoutes);
+  app.use("/api/explore", exploreRoutes);
+};
 module.exports = InitRoutes;
