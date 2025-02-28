c02d3a875976101adf1f59cbd4f14ef6bd872319
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-28 11:15:34 +0700

    format

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,51 +1,51 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const Post = require('../models/Post');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const Post = require("../models/Post");
 
 const authMiddleware = {
-    verifyToken : async (req, res, next) => {
-        const token = req.headers.token;
-        if (token) {
-          const accessToken = token.split(" ")[1];
-          jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) {
-              return res.status(401).json("Token is not valid");
-            }
-            req.user = user;
-            next();
-          });
-        } else {
-          return res.status(401).json("You are not authenticated");
+  verifyToken: async (req, res, next) => {
+    const token = req.headers.token;
+    if (token) {
+      const accessToken = token.split(" ")[1];
+      jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+          return res.status(401).json("Token is not valid");
         }
-      },
-      verifyTokenAndUserAuthorization: (req, res, next) => {
-        authMiddleware.verifyToken(req, res, () => {
-          if (req.user.id === req.params.id || req.user.isAdmin) {
-            next();
-          } else {
-            return res.status(403).json("You're not allowed to do that!");
-          }
-        });
-      },
-      verifyTokenAndUserPostAuthorization: async (req, res, next) => {
-        authMiddleware.verifyToken(req, res, async () => {
-            try {
-                const post = await Post.findById(req.params.id);
-                if (!post) {
-                    return res.status(404).json("Post not found");
-                }
-                console.log(req.user.isAdmin);
-                if (post.user.toString() === req.user.id || req.user.isAdmin) {
-                    next();
-                } else {
-                    return res.status(403).json("You're not allowed to do that!");
-                }
-            } catch (error) {
-                return res.status(500).json("Internal Server Error");
-            }
-        });
-    },
-     verifyTokenAndCommentAuthorization: (req, res, next) => {
+        req.user = user;
+        next();
+      });
+    } else {
+      return res.status(401).json("You are not authenticated");
+    }
+  },
+  verifyTokenAndUserAuthorization: (req, res, next) => {
+    authMiddleware.verifyToken(req, res, () => {
+      if (req.user.id === req.params.id || req.user.isAdmin) {
+        next();
+      } else {
+        return res.status(403).json("You're not allowed to do that!");
+      }
+    });
+  },
+  verifyTokenAndUserPostAuthorization: async (req, res, next) => {
+    authMiddleware.verifyToken(req, res, async () => {
+      try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+          return res.status(404).json("Post not found");
+        }
+        console.log(req.user.isAdmin);
+        if (post.user.toString() === req.user.id || req.user.isAdmin) {
+          next();
+        } else {
+          return res.status(403).json("You're not allowed to do that!");
+        }
+      } catch (error) {
+        return res.status(500).json("Internal Server Error");
+      }
+    });
+  },
+  verifyTokenAndCommentAuthorization: (req, res, next) => {
     middlewareController.verifyToken(req, res, () => {
       console.log("req.user.id: " + req.user.id);
       console.log("ownerId: :" + req.body.ownerId);
@@ -60,6 +60,5 @@ const authMiddleware = {
       }
     });
   },
-      
-}
+};
 module.exports = authMiddleware;
