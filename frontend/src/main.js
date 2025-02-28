e2fb3f2f1154de03f4aeafc355f3476fa3c667d1
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-28 11:17:15 +0700

    format

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router/index'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 // Import Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
