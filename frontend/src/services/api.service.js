fa0b6a8e53f1138ca33051593763b12fd39399df
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-28 16:48:55 +0700

    .evn add

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://projectnlcntt-production.up.railway.app',
   withCredentials: true,
 })
 
