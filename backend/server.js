b34115cd808de89b9d413e60dc56b4ec7526fa6e
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-02-28 16:48:39 +0700

    config cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173"|| "https://projectnlcntt-production.up.railway.app",
     credentials: true,
   })
 );
