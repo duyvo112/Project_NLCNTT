11ad9d4c0bc595d7c08fa89be4c4d79d1e944580
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-03-01 09:34:56 +0700

    đổi domain

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',//test nào xog đổi lại
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 })
