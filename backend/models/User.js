d48821fa44256bfd3b6122693f8a0590c69cf560
duyvo112 <duyb2111833@student.ctu.edu.vn>
2025-03-02 23:18:35 +0700

    bỏ unique fullname

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  fullname: { type: String, required: true, unique: true },
+  fullname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  about: { type: String, default: "I'm a new user" }, // Giới thiệu bản thân
-  address: { type: String, default: "" }, // Địa chỉ
-  phone: { type: String, default: "" }, // Số điện thoại
+  about: { type: String, default: "I'm a new user" },
+  address: { type: String, default: "" },
+  phone: { type: String, default: "" },
   avatar: {
     type: String,
     default:
       "https://t4.ftcdn.net/jpg/00/97/00/09/240_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg",
-  }, // Ảnh đại diện
-  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Danh sách bạn bè
-  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Lời mời kết bạn
+  },
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   isAdmin: {
     type: Boolean,
     default: false,
